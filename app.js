3b3f8425c1681f1b97e2d129929076030ec1baff
maruyan <7115497+hmaruyama@users.noreply.github.com>
2019-06-26 17:31:47 +0900

    Fixed App

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+const PORT = process.env.PORT || 3000
 
 // HTTPリクエストを受け取る部分
 app.get('/', function (req, res) {
@@ -12,10 +12,10 @@ app.get('/user', function (req, res) {
 });
 
 // サーバーを起動する部分
-var server = app.listen(3000, function () {
+var server = app.listen(PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, PORT);
 });
 
 module.exports = app;
